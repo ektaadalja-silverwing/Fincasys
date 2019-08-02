fea80cf7ac0f17407f59771994077222593957be
ekta <ektaadalja.silverwing@gmail.com>
2019-08-02 17:48:20 +0530

    2/8/2019 aa photos activity 3:05 PM

---
+++ app/src/main/java/com/example/fincasys/activity/CongratulationActivity.java
@@ -0,0 +1,15 @@
+package com.example.fincasys.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.fincasys.R;
+
+public class CongratulationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_congratulation);
+    }
+}
